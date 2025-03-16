d95ca811ddde270ffff69e1252b3010d4d88d120
Anssi <apaivinen@gmail.com>
2025-03-16 19:19:39 +0200

    added vimrc

---
+++ .vimrc
@@ -0,0 +1,2 @@
+set number
+highlight LineNr term=bold cterm=NONE ctermfg=DarkGrey ctermbg=NONE gui=NONE guifg=#b4b3f2 guibg=NONE
--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# This is useless file and cba to actually create a proper script
 
 #sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y zsh git vim wget
-
-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-mkdir -p ~/.config/tmux/plugins/catppuccin
-git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
-
-cp ./.tmux.conf ~/.tmux.conf
-cp ./.zshrc ~/.zshrc
-cp ./oh-my-zsh-theme/anssi.zsh-theme ~/.oh-my-zsh/themes/anssi.zsh-theme
-
-sudo chsh -s `which zsh`
+# sudo apt install -y zsh git vim wget
+# 
+# zsh
+# sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# 
+# mkdir -p ~/.config/tmux/plugins/catppuccin
+# git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+# 
+# cp ./.tmux.conf ~/.tmux.conf
+# cp ./.zshrc ~/.zshrc
+# cp ./oh-my-zsh-theme/anssi.zsh-theme ~/.oh-my-zsh/themes/anssi.zsh-theme
+# 
+# sudo chsh -s `which zsh`
